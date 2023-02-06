86b8139f7141417403bb55c5dc4bb1e642d645e5
Priyanka Chatterjee <91906722+dollpriyanka@users.noreply.github.com>
2023-02-06 10:33:13 +0000

    add: code

--- client/src/components/FileUploads.js
+++ client/src/components/FileUploads.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import Message from './message';
 import axios from 'axios';
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('No file chosen');
     const [uploadedFile, setUploadedFile] = useState({});
+    const [message, setMessage] = useState('');
 
     const onChange = e => {
         setFile(e.target.files[0]);
@@ -14,38 +16,46 @@ const FileUpload = () => {
     const onSubmit = async e => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append('file',file);
+        formData.append('file', file);
 
         try {
-           const res = await axios.post('/Upload',formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            const { fileName, filePath } = res.data;
+
+            setUploadedFile({ fileName, filePath });
+            setMessage('File Uploaded');
+        } catch (err) {
+            if (err.response.status === 500) {
+                setMessage("There was a problem with the server");
+            } else {
+                setMessage(err.response.data.msg);
             }
-           });
-
-           const {fileName, filePath } = res.data;
-
-           setUploadedFile({fileName,filePath});
-        }catch(err) {
-                   if(err.response.status === 500) {
-                    console.log("There was a problem with the server");
-                   } else {
-                    console.log(err.response.data.msg);
-                   }
         }
     }
 
     return (
         <Fragment>
+            {message ? <Message msg={message} /> :null }
             <form onSubmit={onSubmit}>
                 <div className='custom-file mb-4'>
                     <label class="form-label" htmlFor="customFile">{filename}</label>
-                    <input type="file" class="form-control" id="customFile"onChange={onChange} />
+                    <input type="file" class="form-control" id="customFile" onChange={onChange} />
                 </div>
 
 
                 <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
             </form>
+            {uploadedFile ? <div className="row mt-5">
+                <div className="col-md-6 m-auto">
+                    <h3 className="text-center">{ uploadedFile.fileName }</h3>
+                    <img src={uploadedFile.filePath} alt="" style={{width: '100%'}} />
+                </div>
+            </div> : null }
         </Fragment>
     );
 };
