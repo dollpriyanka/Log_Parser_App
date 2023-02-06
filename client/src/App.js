ba24c1a584ad989e223d5f01f7d79e32fbf37e24
Priyanka Chatterjee <91906722+dollpriyanka@users.noreply.github.com>
2023-02-06 07:12:09 +0000

    add: code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className='container mt-4'>
+    <h4 className="display-4 text-center mb-4">
+      <i className="fad fa-react"></i> React File Upload
+    </h4>
+  </div>
+);
+
+
+export default App;
