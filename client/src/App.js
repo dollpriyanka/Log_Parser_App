abd4111735a072c72ecc79b81f465a7357c979cc
Priyanka Chatterjee <91906722+dollpriyanka@users.noreply.github.com>
2023-02-07 16:05:33 +0000

    add: complete

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => (
   <div className='container mt-4'>
     <h4 className="display-4 text-center mb-4">
-      <i className="fad fa-react"></i> React File Upload
+      <i>React File Upload</i>
     </h4>
 
     <FileUpload />
